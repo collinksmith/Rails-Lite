988255f418df46fb15550fd02d40364975c8997f
Collin Smith <collink33@gmail.com>
2015-07-21 13:55:19 -0700

    Add basic webrick server implementation

--- bin/p01_basic_server.rb
+++ bin/p01_basic_server.rb
@@ -1,5 +1,19 @@
 require 'webrick'
 
+server = WEBrick::HTTPServer.new(:Port => 3000)
+
+server.mount_proc("/") do |req, res|
+  res.content_type = "text/text"
+  text = req.path
+  res.body = text
+end
+
+trap('INT') do
+  server.shutdown
+end
+
+server.start
+
 # http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick.html
 # http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPRequest.html
 # http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPResponse.html
