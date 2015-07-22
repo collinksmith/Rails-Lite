0535bf1849c0249efce6dcaccd848d8b825352c3
Collin Smith <collink33@gmail.com>
2015-07-21 17:42:31 -0700

    Add functionality to create a nested hash - TODO: merge nested hashes

--- lib/phase5/params.rb
+++ lib/phase5/params.rb
@@ -1,4 +1,5 @@
 require 'uri'
+require 'byebug'
 
 module Phase5
   class Params
@@ -10,9 +11,19 @@ module Phase5
     # You haven't done routing yet; but assume route params will be
     # passed in as a hash to `Params.new` as below:
     def initialize(req, route_params = {})
+      @params = {}
+      parse_www_encoded_form(req.query_string) unless req.query_string.nil?
+      parse_www_encoded_form(req.body) unless req.body.nil?
+
+      @params.merge!(route_params)
+
+      p @params
+
+      @params
     end
 
     def [](key)
+      @params[key.to_s]
     end
 
     # this will be useful if we want to `puts params` in the server log
@@ -29,11 +40,44 @@ module Phase5
     # should return
     # { "user" => { "address" => { "street" => "main", "zip" => "89436" } } }
     def parse_www_encoded_form(www_encoded_form)
+      key_value_pairs = URI.decode_www_form(www_encoded_form)
+
+      key_value_pairs.each do |pair|
+        keys = parse_key(pair.first)
+        value = pair.last
+
+        h = set_hash(keys, value)
+        p h
+        @params.merge!(h)
+      end
     end
+
+  def set_hash(keys, value)
+    hash = {}
+    keys.map!(&:to_s)
+
+    keys.length.times do |i|
+      eval_string = "hash"
+      (0..i).each do |j|
+        eval_string << "['#{keys[j]}']"
+      end
+
+      if i == (keys.length - 1)
+        eval_string << " = value"
+        eval(eval_string)
+      else
+        eval_string << " = {}"
+        eval(eval_string)
+      end
+    end
+
+    hash
+  end
 
     # this should return an array
     # user[address][street] should return ['user', 'address', 'street']
     def parse_key(key)
+      key.split(/\]\[|\[|\]/)
     end
   end
 end
